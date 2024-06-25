46560421854bcc4342df5e8ab915d798dff41822
x-koko <kokofiziowy@gmail.com>
2024-06-25 20:16:00 +0200

    Added blog

--- src/dzien2_backend/src/lib.rs
+++ src/dzien2_backend/src/lib.rs
@@ -1,4 +1,24 @@
+use std::cell::RefCell;
+
+thread_local! {
+    static WPISY: RefCell<Vec<String>> = RefCell::default();
+}
+
 #[ic_cdk::query]
-fn greet(name: String, last_name: u8) -> String {
+fn greet(name: String, last_name: i8) -> String {
     format!("Hello, {} {}!", name, last_name)
+}
+
+#[ic_cdk::update]
+fn dodaj_wpis(wpis: String) {
+    WPISY.with(|wpisy| {
+        wpisy.borrow_mut().push(wpis)
+    });
+}
+
+#[ic_cdk::query]
+fn odczytaj_wpisy() -> Vec<String> {
+    WPISY.with(|wpisy: &RefCell<Vec<String>>| {
+        wpisy.borrow().clone()
+    })
 }
